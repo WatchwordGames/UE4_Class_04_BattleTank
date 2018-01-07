60c6e1682cffee79abcf4f7f76c1017f637c1e35
Smoke <this@isafakeemail.com>
2018-01-06 23:46:26 -0800

    Added crosshair world hit location code

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -46,6 +46,47 @@ void ATankPlayerController::AimTowardsCrosshair()
 
 bool ATankPlayerController::GetSightRayHitLocation(FVector& HitLocation) const
 {
-	
+	// Get the crosshair position
+	int32 ViewportSizeX;
+	int32 ViewportSizeY;
+
+	GetViewportSize(ViewportSizeX, ViewportSizeY);
+
+	FVector2D ScreenLocation = { ViewportSizeX * CrosshairXPosition, ViewportSizeY * CrosshairYPosition };
+
+	// Deproject to world direction
+	FVector ScreenWorldDirection;
+	if (GetLookDirection(ScreenLocation, ScreenWorldDirection))
+	{
+		// Line trace along the look direction to see where we hit
+		return GetLookVectorHitLocation(ScreenWorldDirection, HitLocation);
+	}
+
+
 	return false;
+}
+
+bool ATankPlayerController::GetLookDirection(FVector2D ScreenLocation, FVector& ScreenWorldDirection) const
+{
+	FVector ScreenWorldLocation;
+	return DeprojectScreenPositionToWorld(ScreenLocation.X, ScreenLocation.Y, ScreenWorldLocation, ScreenWorldDirection);
+}
+
+bool ATankPlayerController::GetLookVectorHitLocation(FVector LookDirection, FVector& HitLocation) const
+{
+	float LineTraceRangeInCentimeters = 100000;
+	FHitResult HitResult;
+	
+	FVector CameraViewPointLocation = PlayerCameraManager->GetCameraLocation();
+	
+	if (GetWorld()->LineTraceSingleByChannel(HitResult, CameraViewPointLocation, CameraViewPointLocation + (LookDirection * LineTraceRangeInCentimeters), ECollisionChannel::ECC_Visibility))
+	{
+		HitLocation = HitResult.Location;
+		return true;
+	}
+	else
+	{
+		HitLocation = FVector(0);
+		return false;
+	}
 }
--- BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -15,9 +15,17 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
+private:
+	UPROPERTY(EditAnywhere)
+	float CrosshairXPosition = 0.5;
+	UPROPERTY(EditAnywhere)
+	float CrosshairYPosition = 0.333333;
+	bool GetLookVectorHitLocation(FVector LookDirection, FVector& HitLocation) const;
+
 public:
 	ATank* GetControlledTank() const;
 	void AimTowardsCrosshair();
+	bool GetLookDirection(FVector2D ScreenLocation, FVector& ScreenWorldDirection) const;
 	bool GetSightRayHitLocation(FVector& HitLocation) const;
 	virtual void BeginPlay() override;
 	virtual void Tick(float DeltaTime) override;
