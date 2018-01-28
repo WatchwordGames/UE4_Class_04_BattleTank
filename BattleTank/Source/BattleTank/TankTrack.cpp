b6c1f172917020195a13729c6fd4bfdcda803e3c
Smoke <this@isafakeemail.com>
2018-01-28 14:53:47 -0800

    Added a sideways force at tracks to prevent sideways slippage (since tanks don't move like that.) It causes lots of other movement issues but will correct these later

--- BattleTank/Source/BattleTank/TankTrack.cpp
+++ BattleTank/Source/BattleTank/TankTrack.cpp
@@ -2,6 +2,27 @@
 
 #include "TankTrack.h"
 
+UTankTrack::UTankTrack()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UTankTrack::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// Speed along right vector of tank
+	float SlippageSpeed = FVector::DotProduct(GetRightVector(), GetComponentVelocity());
+	
+	// Corrective acceleration for slippage
+	FVector CorrectionAcceleration = -(SlippageSpeed / DeltaTime) * GetRightVector();
+
+	UPrimitiveComponent* TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+
+	FVector CorrectionForcePerTrack = ((TankRoot->GetMass()) * CorrectionAcceleration) / 2.0f;
+
+	TankRoot->AddForce(CorrectionForcePerTrack);
+}
 
 void UTankTrack::SetThrottle(float Throttle)
 {
--- BattleTank/Source/BattleTank/TankTrack.h
+++ BattleTank/Source/BattleTank/TankTrack.h
@@ -12,6 +12,12 @@ class BATTLETANK_API UTankTrack : public UStaticMeshComponent
 {
 	GENERATED_BODY()
 	
+private:
+
+	UTankTrack();
+
+	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) override;
+
 public: 
 	// Sets a throttle between -1 and +1
 	UFUNCTION(BlueprintCallable, Category = "Input")
